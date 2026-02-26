38538ef9186bca7c0440495fecf66592e9cf3653
Daniel Mazzeu Dev <danielmazzeu.dev@gmail.com>
2026-02-25 22:22:30 -0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import Main from "./components/Main/Main";
 export default function Home() {
     return (
         <>
-            <Header />
+            {/* <Header /> */}
             <Main />
-            <Footer />
+            {/* <Footer /> */}
         </>
     );
 }
